dabbea110f5b3ee12aa5da5c9262c54daaf12d83
도의준 <118966238+gedfsg@users.noreply.github.com>
2026-03-28 20:01:34 +0900

    2001

--- Assets/Scripts/DestructibleObstacle.cs
+++ Assets/Scripts/DestructibleObstacle.cs
@@ -1,4 +1,3 @@
-using System;
 using UnityEngine;
 
 public class DestructibleObstacle : MonoBehaviour
@@ -6,6 +5,7 @@ public class DestructibleObstacle : MonoBehaviour
     public float maxHealth = 100f;
     private float currentHealth;
 
+    // 데미지 수치를 표시할 UI 프리팹 객체임.
     [Header("Damage UI")]
     public GameObject damageTextPrefab;
 
@@ -18,6 +18,7 @@ public class DestructibleObstacle : MonoBehaviour
     {
         currentHealth -= damage;
 
+        // 데미지 텍스트를 생성하는 함수를 호출함.
         ShowDamageText(damage);
 
         if (currentHealth <= 0)
@@ -26,15 +27,21 @@ public class DestructibleObstacle : MonoBehaviour
         }
     }
 
+    // 인스턴스화 과정을 처리하는 내부 함수임.
     private void ShowDamageText(float amount)
     {
-        if(damageTextPrefab != null)
+        if (damageTextPrefab != null)
         {
-            Vector3 randomOffset = new Vector3(UnityEngine.Random.Range(-0.5f, 0.5f), 1f, UnityEngine.Random.Range(-0.5f, 0.5f));
+            // 모호한 참조 에러 방지를 위해 UnityEngine.Random을 명시적으로 호출함.
+            float randomX = UnityEngine.Random.Range(-0.5f, 0.5f);
+            float randomZ = UnityEngine.Random.Range(-0.5f, 0.5f);
+            Vector3 randomOffset = new Vector3(randomX, 1f, randomZ);
+            
             GameObject textObj = Instantiate(damageTextPrefab, transform.position + randomOffset, Quaternion.identity);
-
+            
             DamageText damageText = textObj.GetComponent<DamageText>();
-            if (damageText != null)            {
+            if (damageText != null)
+            {
                 damageText.Setup(amount);
             }
         }
--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -9,22 +9,46 @@ public class Health : MonoBehaviour
     public UnityEvent onDeath;
     public UnityEvent onHurt;
 
+    // 데미지 수치를 표시할 UI 프리팹 객체임.
     [Header("Damage UI")]
     public GameObject damageTextPrefab;
+
     void Awake()
     {
         currentHealth = maxHealth;
     }
 
-    public void TakeDamage(float damage)
+    public void TakeDamage(float amount)
     {
-        currentHealth -= damage;
-
-        ShowDamageText(damage);
+        currentHealth -= amount;
+        
+        // 데미지 텍스트를 생성하는 함수를 호출함.
+        ShowDamageText(amount);
+        
         onHurt?.Invoke();
         if (currentHealth <= 0f)
         {
             Die();
+        }
+    }
+
+    // 인스턴스화 과정을 처리하는 내부 함수임.
+    private void ShowDamageText(float amount)
+    {
+        if (damageTextPrefab != null)
+        {
+            // 모호한 참조 에러 방지를 위해 UnityEngine.Random을 명시적으로 호출함.
+            float randomX = UnityEngine.Random.Range(-0.5f, 0.5f);
+            float randomZ = UnityEngine.Random.Range(-0.5f, 0.5f);
+            Vector3 randomOffset = new Vector3(randomX, 1f, randomZ);
+            
+            GameObject textObj = Instantiate(damageTextPrefab, transform.position + randomOffset, Quaternion.identity);
+            
+            DamageText damageText = textObj.GetComponent<DamageText>();
+            if (damageText != null)
+            {
+                damageText.Setup(amount);
+            }
         }
     }
 
@@ -46,19 +70,5 @@ public class Health : MonoBehaviour
     public float GetMaxHealth()
     {
         return maxHealth;
-    }
-
-    private void ShowDamageText(float damage)
-    {
-        if(damageTextPrefab != null)
-        {
-            Vector3 randomOffset = new Vector3(Random.Range(-0.5f, 0.5f), 1f, Random.Range(-0.5f, 0.5f));
-            GameObject textObj = Instantiate(damageTextPrefab, transform.position + randomOffset, Quaternion.identity);
-
-            DamageText damageText = textObj.GetComponent<DamageText>();
-            if (damageText != null)            {
-                damageText.Setup(damage);
-            }
-        }
     }
 }
